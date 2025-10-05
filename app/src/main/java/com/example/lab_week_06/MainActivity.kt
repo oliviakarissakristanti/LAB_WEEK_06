c0336c9e3cf6bca1d4fa646a1aaa3f85611184b2
oliviakarissakristanti <koliviakarissa@gmail.com>
2025-10-05 15:21:25 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

---
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.lab_week_06
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab_week_06.model.CatModel
+import com.example.lab_week_06.model.CatBreed
+import com.example.lab_week_06.model.Gender
+
+class MainActivity : AppCompatActivity() {
+
+    private val recyclerView: RecyclerView by lazy { findViewById(R.id.recycler_view) }
+    private val catAdapter by lazy { CatAdapter(layoutInflater, GlideImageLoader(this)) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = catAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        catAdapter.setData(
+            listOf(
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Fred", "Silent and deadly", "https://cdn2.thecatapi.com/images/7dj.jpg"),
+                CatModel(Gender.Female, CatBreed.ExoticShorthair, "Wilma", "Cuddly assassin", "https://cdn2.thecatapi.com/images/egv.jpg"),
+                CatModel(Gender.Unknown, CatBreed.AmericanCurl, "Curious George", "Award winning investigator", "https://cdn2.thecatapi.com/images/bar.jpg")
+            )
+        )
+    }
+}
